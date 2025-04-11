8f0483276469affcfe350d08e7a3ce3b77284857
syedaeliya <aeliya.git@gmail.com>
2025-04-12 00:25:39 +0530

    Initial Commit

---
+++ src/test/java/project/syed/SpringAI/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.syed.SpringAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
